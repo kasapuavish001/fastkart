d9c1c0984e0e593318da2985de4f5fea3c42f100
Kasapu_avish <141611207+kasapuavish001@users.noreply.github.com>
2023-10-23 12:03:37 +0530

    Update index.jsx

--- Components/HeaderTop/index.jsx
+++ Components/HeaderTop/index.jsx
@@ -16,17 +16,17 @@ const index = () => {
     {
       id: "1",
       name: "English",
-      url: "/../public/assests/images/united-kingdom.PNG",
+      url: "images/united-kingdom.png",
     },
     {
       id: "2",
       name: "Germany",
-      url: "/../public/assests/images/germany.PNG",
+      url: "/../public/assests/images/germany.png",
     },
     {
       id: "3",
       name: "Turkish",
-      url: "/../public/assests/images/turkish.PNG",
+      url: "/../public/assests/images/turkish.png",
     },
   ];
 
